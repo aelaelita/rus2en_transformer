ecc1e3f1e0c9c3c3206357aae9d20e022bcccb81
Alek Salikhzyanov <a.salikhzyanov@innopolis.ru>
2020-03-13 01:51:40 +0300

    Update model training

--- NMT.py
+++ NMT.py
@@ -1,12 +1,13 @@
-from torch import nn, optim, tensor
+from torch import nn, optim, tensor, device
 
 from neural_machine_translation import NMTEncoder, NMTDecoder
-from prepare_data import get_iterators
+from prepare_data import NMTDataset
 
 BATCH_SIZE = 128
 
 
-def train(data, encoder, decoder, criterion, e_optimizer, de_optimizer, debug_steps=100, epoch=-1):
+def train(data, encoder, decoder, criterion, e_optimizer, de_optimizer, tag_ix, debug_steps=100, epoch=-1,
+          device=device('cpu')):
     encoder.train(True)
     decoder.train(True)
     e_optimizer.zero_grad()
@@ -16,33 +17,46 @@ def train(data, encoder, decoder, criterion, e_optimizer, de_optimizer, debug_st
     for i, batch in enumerate(data):
         src = batch.Russian
         trg = batch.English
+        if src.shape[0] != BATCH_SIZE:
+            continue
+        loss = 0
         encoder_output, hidden = encoder(src, hidden)
-        decoder_input = tensor()
+        decoder_input = tensor([[tag_ix]] * BATCH_SIZE)
         for t in range(1, trg.size(1)):
-            output, hidden = decoder(decoder_input, hidden, encoder_output)
-        loss = criterion(output, trg)
+            print(t)
+            if t == 45:
+                print(t)
+            out, hidden = decoder(decoder_input.to(device),
+                                  hidden.to(device),
+                                  encoder_output.to(device))
+            truth = trg[:, t]
+            loss += criterion(out.to(device), truth.to(device))
+            decoder_input = trg[:, t].unsqueeze(1)
+
         loss.backward()
         e_optimizer.step()
         de_optimizer.step()
         ovr_loss += loss.item()
         if i % debug_steps == 0:
-            print(f'Epoch {epoch} ({i}/{len(data)}): loss={ovr_loss / i}')
+            print(f'Epoch {epoch} ({i}/{len(data)}): loss={ovr_loss / i + 1}')
 
 
 def main():
     learning_rate = 1e-3
     hidden_dim = 256
 
-    train_data, validation_data, input_dim, output_dim = get_iterators('data/corpus.en_ru.1m.en',
-                                                                       'data/corpus.en_ru.1m.ru',
-                                                                       batch_size=BATCH_SIZE, debug=True)
+    dataset = NMTDataset('data/corpus.en_ru.1m.en',
+                         'data/corpus.en_ru.1m.ru')
+
+    train_data, validation_data = dataset.get_iterators(batch_size=BATCH_SIZE, debug=True)
+    input_dim, output_dim = dataset.INPUT_DIM, dataset.OUTPUT_DIM
 
     encoder = NMTEncoder(input_dim, hidden_dim)
     decoder = NMTDecoder(hidden_dim, output_dim)
-    criterion = nn.NLLLoss()
+    criterion = nn.CrossEntropyLoss()
     e_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
     de_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
-    train(train_data, encoder, decoder, criterion, e_optimizer, de_optimizer)
+    train(train_data, encoder, decoder, criterion, e_optimizer, de_optimizer, dataset.get_src_tag_idx('<eos>'))
 
 
 if __name__ == '__main__':
