0ac8990f5f735420a58497a24a5382dc6e09f54a
rumblingturtle <edzaljaev@gmail.com>
2020-03-13 22:08:18 +0300

    added evaluation and saving

--- NMT.py
+++ NMT.py
@@ -45,6 +45,50 @@ def train(data, encoder, decoder, criterion, e_optimizer, de_optimizer, tag_ix, 
               torch.save(encoder.state_dict(), save_path+"/encoder.pth")
               torch.save(decoder.state_dict(), save_path+"/decoder.pth")
 
+import gc
+def test(data, encoder, decoder,test_path,tag_ix):
+  with open(test_path) as fp:
+    test_lines = fp.readlines()
+  
+  tokens = []
+  hidden = encoder.init_hidden(1)
+  with torch.no_grad():
+    for line in test_lines:
+      tokens = data._tokenize_ru(line)
+      tokens = list(map(data.get_src_tag_idx,tokens))
+      tokens = [data.get_src_tag_idx('<sos>')]+tokens+[data.get_src_tag_idx('<eos>')]
+      print(tokens)
+      if len(tokens)<data.INPUT_DIM:
+        tokens+=[0]*(dataset.INPUT_DIM-len(tokens))
+      
+      t = torch.tensor([tokens])
+      print(t)
+      encoder_output, hidden = encoder(t, hidden)
+      decoder_input = tensor([[tag_ix]] * 1)
+      predicted = []
+      MAX_ITER = 25
+      i = 0
+      while(i!=MAX_ITER):
+        print(i)
+        i+=1
+        decoder_input = decoder_input.type(torch.LongTensor)
+        decoder_input[decoder_input<0] = 0
+
+        out, hidden = decoder(decoder_input,
+                              hidden,
+                              encoder_output)
+        out = out[0][i]
+        
+        pred_token = int(out.item())
+        predicted.append(data.get_trg_tag_from_idx(pred_token))
+        if pred_token == data.get_src_tag_idx('<eos>'):
+          break
+        decoder_input = out.unsqueeze(0).unsqueeze(0)
+
+      print(predicted)
+      
+      with open('out.txt', 'a') as f:
+        f.write(" ".join(predicted)+"\n")
 
 def main():
     learning_rate = 1e-3
@@ -62,6 +106,11 @@ def main():
     e_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
     de_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
     train(train_data, encoder, decoder, criterion, e_optimizer, de_optimizer, dataset.get_src_tag_idx('<eos>'),".")
+
+    
+    #encoder.load_state_dict(torch.load("encoder.pth"))
+    #decoder.load_state_dict(torch.load("decoder.pth"))
+    test(dataset, encoder, decoder,"data/eval-ru-100.txt", dataset.get_src_tag_idx('<eos>'))
 
 
 if __name__ == '__main__':
