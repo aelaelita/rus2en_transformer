2458b385745d582889ca3d3c8b4f8145471b675f
rumblingturtle <edzaljaev@gmail.com>
2020-03-13 05:52:54 +0300

    Mem alloc fix and model saving

--- NMT.py
+++ NMT.py
@@ -3,37 +3,47 @@ from torch import nn, optim, tensor, device
 from neural_machine_translation import NMTEncoder, NMTDecoder
 from prepare_data import NMTDataset
 
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 
-
-def train(data, encoder, decoder, criterion, e_optimizer, de_optimizer, tag_ix, debug_steps=1, epoch=-1,
+def train(data, encoder, decoder, criterion, e_optimizer, de_optimizer, tag_ix, save_path, debug_steps=10, epoch=10,
           device=device('cpu')):
-    encoder.train(True)
-    decoder.train(True)
-    e_optimizer.zero_grad()
-    de_optimizer.zero_grad()
-    ovr_loss = 0
-    hidden = encoder.init_hidden(BATCH_SIZE)
-    for i, batch in enumerate(data):
-        src = batch.Russian
-        trg = batch.English
-        loss = 0
-        encoder_output, hidden = encoder(src, hidden)
-        decoder_input = tensor([[tag_ix]] * BATCH_SIZE)
-        for t in range(1, trg.size(1)):
-            out, hidden = decoder(decoder_input.to(device),
-                                  hidden.to(device),
-                                  encoder_output.to(device))
-            truth = trg[:, t]
-            loss += criterion(out.to(device), truth.to(device))
-            decoder_input = trg[:, t].unsqueeze(1)
+    for e in range(epoch):
+      encoder.train(True)
+      decoder.train(True)
+      ovr_loss = 0
+      hidden = encoder.init_hidden(BATCH_SIZE)
+      for i, batch in enumerate(data):
+          print(i)
+          e_optimizer.zero_grad()
+          de_optimizer.zero_grad()
+          src = batch.Russian.to(device)
+          trg = batch.English.to(device)
 
-        loss.backward()
-        e_optimizer.step()
-        de_optimizer.step()
-        ovr_loss += loss.item()
-        if i % debug_steps == 0:
-            print(f'Epoch {epoch} ({int(i/debug_steps)}/{len(data)}): loss={ovr_loss / (i + 1)}')
+          if src.shape[0] != BATCH_SIZE:
+              continue
+
+          loss = 0
+          encoder_output, hidden = encoder(src, hidden)
+          decoder_input = tensor([[tag_ix]] * BATCH_SIZE)
+          
+
+          for t in range(1, trg.size(1)):
+              
+              out, hidden = decoder(decoder_input,
+                                    hidden,
+                                    encoder_output)
+              
+              loss += criterion(out, trg[:, t])
+              decoder_input = trg[:, t].unsqueeze(1)
+
+          loss.backward()
+          e_optimizer.step()
+          de_optimizer.step()
+          ovr_loss += loss.item()
+          if i % debug_steps == 0:
+              print(f'Epoch {e}: loss={ovr_loss / (i + 1)}')
+              torch.save(encoder.state_dict(), save_path+"/encoder.pth")
+              torch.save(decoder.state_dict(), save_path+"/decoder.pth")
 
 
 def main():
@@ -51,7 +61,7 @@ def main():
     criterion = nn.CrossEntropyLoss()
     e_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
     de_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
-    train(train_data, encoder, decoder, criterion, e_optimizer, de_optimizer, dataset.get_src_tag_idx('<eos>'))
+    train(train_data, encoder, decoder, criterion, e_optimizer, de_optimizer, dataset.get_src_tag_idx('<eos>'),".")
 
 
 if __name__ == '__main__':
